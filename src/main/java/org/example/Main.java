e09ae6ecc5d237846540699eeae17e96db19ff68
Ilya Dudelzak <65915836+IlyaDudelzak@users.noreply.github.com>
2026-02-03 15:28:17 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    static void printName() {
+        System.out.println("{\"name\": \"Ilya\", \"lastName\": \"Dudelzak\"}");
+    }
+    static void main() {
+        printName();
+    }
+}
